3dec8eae104e0c686be26d264bd2a10e8b618f23
IKBOstudent <maxpon225@gmail.com>
2023-08-28 00:57:35 +0300

    fix: ts fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import * as path from 'path';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-    plugins: [svgr(), eslint(), react()],
+    plugins: [svgr(), eslint(), react(), tsconfigPaths()].filter(Boolean),
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     },
